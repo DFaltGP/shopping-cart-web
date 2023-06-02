419fd1bdd6909a9d6cf1386abae9e72467d32d76
DFaltGP <skyzzlockzz@gmail.com>
2023-06-02 19:31:54 -0300

    Keep working...

--- src/components/ShopCard.tsx
+++ src/components/ShopCard.tsx
@@ -2,7 +2,7 @@ import Image from 'next/image'
 
 export default function ShopCard() {
   return (
-    <div className="items-center flex w-3/12 flex-col justify-between gap-4 rounded-md bg-purple-700 p-4">
+    <div className="items-center flex w-3/12 flex-col justify-between gap-4 rounded-md bg-[#18181b] p-4">
       <div className="flex w-full flex-col -items-center justify-center gap-4">
         <Image
           src={'/images/jett.jpg'}
@@ -19,10 +19,10 @@ export default function ShopCard() {
       <div className="flex w-full flex-wrap items-start justify-between gap-4  text-white">
         <p className="font-semibold">R$ 19,90</p>
         <div className="items-center flex flex-col flex-wrap gap-4  text-white">
-          <button className="w-32 rounded-md bg-zinc-950 p-2 text-white">
+          <button className="w-32 rounded-md bg-[#9333EA] p-2 text-white">
             ADICIONAR
           </button>
-          <button className="w-32 rounded-md bg-zinc-950 p-2 text-white">
+          <button className="w-32 rounded-md bg-[#9333EA] p-2 text-white">
             COMPRAR
           </button>
         </div>
